1023ffc45c459caaed3d1ad5b40559f74179f4a1
sriram-rao <sriram.rao04@gmail.com>
2013-08-20 00:18:50 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NitkRugSite::Application.config.secret_key_base = '7b570103385841d13c7cbe991dcc0b533adc30b3d0477c41a5e874a12b9e809049f0821b9aa4ce3b3567d264da232448d6f937979d50b71b1cce0bd4c6f87d18'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NitkRugSite::Application.config.session_store :cookie_store, key: '_nitk-rug-site_session'
