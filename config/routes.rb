787ee9c059ac23b75b9105e6c4af6a309cc8fa04
sriram-rao <sriram.rao04@gmail.com>
2013-08-25 01:35:47 +0530

    Major blunder fixed

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ NitkRugSite::Application.routes.draw do
   # get  "static_pages/home"
   get "/about" => 'static_pages#about'
   get "/members" => "static_pages#members"
+  get "/home" => "static_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
