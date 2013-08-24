1e1da7715aa25ecacf0983c8c2ea97a41fc292b8
sriram-rao <sriram.rao04@gmail.com>
2013-08-25 00:31:46 +0530

    Removed credentials

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -76,7 +76,7 @@ class PostsController < ApplicationController
 
     def authenticate
     authenticate_or_request_with_http_basic do |name, password|
-      name == "admin" && password == "secrets"
+      name == "" && password == ""
     end
   end
 end
